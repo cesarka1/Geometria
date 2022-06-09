c8c9eda497d52d2f941ab8e9a49a0c131f6d6142
Nitierk <105940525+Nitierk@users.noreply.github.com>
2022-06-09 19:51:28 -0300

    Criação das Classes em Retangulo

--- GeometriaAPI/Models/Retangulo.cs
+++ GeometriaAPI/Models/Retangulo.cs
@@ -5,8 +5,37 @@ using System.Threading.Tasks;
 
 namespace GeometriaAPI.Models
 {
-    public class Retangulo
+    public class Retangulo : Geometria, IGeometriaFormula
     {
-        
+        public double CalcularArea()
+        {
+            double basev;
+            double alturav;
+            try
+            {
+                basev = double.Parse(m[0]);
+                alturav = double.Parse(m[1]);
+            }
+            catch (System.Exception)
+            {
+                return -1;
+            }
+            return basev*alturav;
+        }
+    public CalcularPerimetro()
+    {
+        double basev;
+        double alturav;
+        try
+        {
+            basev = double.Parse(m[0]);
+            alturav = double.Parse(m[1]);
+        }
+        catch (System.Exception)
+        {
+            return -1;
+        }
+        return (basev*2) + (alturav*2);
+    }
     }
 }
