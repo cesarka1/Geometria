d281db3ad3ee8ea50e47e354e375cabb853b958a
Nitierk <105940525+Nitierk@users.noreply.github.com>
2022-06-09 19:43:36 -0300

    Criação Models

---
+++ GeometriaAPI/Models/Geometria.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeometriaAPI.Models
+{
+    public class Geometria
+    {
+        double[] medidas;
+    }
+}
---
+++ GeometriaAPI/Models/IGeometriaFormula.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeometriaAPI.Models
+{
+    public interface IGeometriaFormula
+    {
+        double[] CalcularArea(string[] m);
+        double[] CalcularPerimetro(string[] m);
+    }
+}
