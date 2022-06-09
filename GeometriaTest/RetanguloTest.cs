52e82492bf2b3f82a1daf0115a03d8061b262998
Nitierk <105940525+Nitierk@users.noreply.github.com>
2022-06-09 20:04:06 -0300

    Atualização e teste Retangulo

---
+++ GeometriaTest/RetanguloTest.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GeometriaTest
+{
+    [TestClass]
+    public class RetanguloTest
+    {
+        Retangulo retangulo = new Retangulo();
+        [TestMethod]
+        public void RetanguloCalcularAreaTestAccept()
+        {
+            double esperado = 20;
+            string[] medidas = {"4", "5"};
+            double resultado = retangulo.CalcularArea(medidas);
+            Assert.AreEqual(esperado, resultado);
+        }
+        [TestMethod]
+        public void RetanguloCalcularAreaTestcomtexto()
+        {
+            double esperado = -1;
+            string[] medidas = {"m", "5"};
+            double resultado = retangulo.CalcularArea(medidas);
+            Assert.AreEqual(esperado, resultado);
+        }
+
+
+    }
+}
