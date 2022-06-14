b18e52339cedc83f9e451fb566ff19eae3b7e79f
Escola <105940525+Nitierk@users.noreply.github.com>
2022-06-14 15:40:18 -0300

    Update Circulo.cs
    *Adicionado Metódos CalculaArea e CalcularPerimetro

--- GeometriaAPI/Models/Circulo.cs
+++ GeometriaAPI/Models/Circulo.cs
@@ -2,11 +2,41 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using 
 
 namespace GeometriaAPI.Models
 {
-    public class Circulo
+    public class Circulo : Geometria, IGeometriaFormula
     {
-        
+        double raio;
+        double pi = 3.1415;
+
+        public dobule CirculoCalcularArea(string[] m)
+        {
+            try
+            {
+                raio = double.Parse(m[0]);
+            }
+            catch (System.Exception)
+            {
+                
+               return -1;
+            }
+            return raio*raio*pi;
+
+        }
+
+        public dobule CirculoCalcular(string[] m)
+        {
+            try
+            {
+                 raio = double.Parse(m[0]);
+            }
+            catch (System.Exception)
+            {
+                 return -1;
+            }
+            return raio*2*pi;
+        }
     }
 }
