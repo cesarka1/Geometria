d2bc5ac14e5341de58daa6939587d590b69a7242
Escola <105940525+Nitierk@users.noreply.github.com>
2022-06-14 14:35:22 -0300

    Update Trapezio.cs
    *Calcular Area
    *Calcular Perimetro

--- GeometriaAPI/Models/Trapezio.cs
+++ GeometriaAPI/Models/Trapezio.cs
@@ -1,7 +1,46 @@
 namespace GeometriaAPI.Models
 {
-    public class Trapezio
+    public class Trapezio : Geometria, IGeometriaFormula
     {
-        
+         public double CalcularArea(string[] m)
+         {
+            double basemenor;
+            double basemaior;
+            double altura;
+
+            try
+            {
+                basemenor = double.Parse(m[0]);
+                basemaior = double.Parse(m[1]);
+                altura = double.Parse(m[2]);
+            }
+            catch (System.Exception)
+            {
+                
+                return -1;
+            }
+            return ((basemaior+basemenor)*altura)/2;
+         }
+
+         public double CalcularPerimetro(string[] m)
+         {
+            double lado1;
+            double lado2;
+            double lado3;
+            double lado4;
+
+            try
+            {
+                lado1 = double.Parse(m[0]);
+                lado2 = double.Parse(m[1]);
+                lado3 = double.Parse(m[2]);
+                lado4 = double.Parse(m[3]);
+            }
+            catch (System.Exception)
+            {
+                return -1;
+            }
+            return (lado1+lado2+lado3+lado4);
+         }
     }
 }
